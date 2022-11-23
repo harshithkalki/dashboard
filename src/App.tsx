6f3309f4658d4817e6c6f753181c3c7ef383e0ac
harshith <harshithkalki@gmail.com>
2022-11-24 00:43:41 +0530

    routes & navbar change

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-// import { HeaderTabs } from "./components/Navbar2";
-import { HeaderResponsive } from "./components/navbar";
+
+import { Home } from "./components/Home";
+import { LoginComp } from "./components/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginComp />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <HeaderResponsive
-        links={[
-          { link: "home", label: "home" },
-          { link: "/home", label: "Profile" },
-          { link: "/login", label: "logout" },
-        ]}
-      />
-      {/* <HeaderTabs tabs={[]} user={{ name: "kalki", image: null }} /> */}
-    </div>
+    <>
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeaderTabs } from "./Navbar2";
+
+export const Home = () => {
+  return (
+    <>
+      <HeaderTabs tabs={[]} user={{ name: "kalki", image: null }} />
+    </>
+  );
+};
